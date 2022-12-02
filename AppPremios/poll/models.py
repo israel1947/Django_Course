1ae528c9dafe25c44ebc86848b5891654fac3ea5
=israel1947 <israeldavidsdvsf@gmail.com>
2022-12-02 12:46:51 +0100

    the models and the files for migrations is created

--- AppPremios/poll/models.py
+++ AppPremios/poll/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+  question_text = models.CharField(max_length=200) #tipo de datos string
+  pub_date = models.DateTimeField("date published")
+  
+class Choise(models.Model):
+  #on_delete=models.CASCADE: elimina la pregunta y todas las respuestas que esta tenga
+  #Question: significa que hereda de la clase Question
+  question = models.ForeignKey(Question,on_delete=models.CASCADE)
+  choise_text = models.CharField(max_length=200)
+  vote = models.IntegerField(default=0)
