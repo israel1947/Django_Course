b086b98cd45410041edc082a36c7dbe6dff6f151
=israel1947 <israeldavidsdvsf@gmail.com>
2022-12-02 13:19:57 +0100

    the firts in  db register is created

--- AppPremios/poll/models.py
+++ AppPremios/poll/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
   question_text = models.CharField(max_length=200) #tipo de datos string
   pub_date = models.DateTimeField("date published")
+  
+  def __str__(self):
+    return self.question_text
+  
+  #definir que una pregunta es reciente cuando es igual o menor a la fecha actual
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
   
 class Choise(models.Model):
   #on_delete=models.CASCADE: elimina la pregunta y todas las respuestas que esta tenga
@@ -10,3 +19,6 @@ class Choise(models.Model):
   question = models.ForeignKey(Question,on_delete=models.CASCADE)
   choise_text = models.CharField(max_length=200)
   vote = models.IntegerField(default=0)
+  
+  def __str__(self):
+    return self.choise_text
